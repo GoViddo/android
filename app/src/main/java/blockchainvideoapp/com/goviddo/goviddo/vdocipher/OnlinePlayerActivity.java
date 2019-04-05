e8877d08d3b533c7ebd23989adec765dc28cd592
Mac <=>
2019-04-05 11:26:23 +0530

    Video View On Server

--- app/src/main/java/blockchainvideoapp/com/goviddo/goviddo/vdocipher/OnlinePlayerActivity.java
+++ app/src/main/java/blockchainvideoapp/com/goviddo/goviddo/vdocipher/OnlinePlayerActivity.java
@@ -1,5 +1,6 @@
 package blockchainvideoapp.com.goviddo.goviddo.vdocipher;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
 import android.os.Build;
@@ -14,16 +15,29 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.vdocipher.aegis.media.ErrorDescription;
 import com.vdocipher.aegis.media.Track;
 import com.vdocipher.aegis.player.VdoPlayer;
 import com.vdocipher.aegis.player.VdoPlayerFragment;
 
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import blockchainvideoapp.com.goviddo.goviddo.R;
+import blockchainvideoapp.com.goviddo.goviddo.activity.HomeActivity;
+import blockchainvideoapp.com.goviddo.goviddo.activity.MainActivity;
+import blockchainvideoapp.com.goviddo.goviddo.coreclass.LoginUserDetails;
 
 public class OnlinePlayerActivity extends AppCompatActivity implements VdoPlayer.InitializationListener {
 
@@ -85,7 +99,58 @@ public class OnlinePlayerActivity extends AppCompatActivity implements VdoPlayer
         disablePlayerUI();
         super.onStop();
 
+
+       LoginUserDetails mLoginUserDetails = new LoginUserDetails(this);
+
+        Log.v("Video ID = ", Utils.vdociper_id );
+        Log.v("User Emaild Id = ", mLoginUserDetails.getEmail());
         Log.v("Total Video Played Time = ",String.valueOf(mVideoPlayedTime));
+
+
+        JSONObject params = new JSONObject();
+        try {
+
+
+            params.put( "vdoCipherId", Utils.vdociper_id );
+            params.put( "userEmailId", mLoginUserDetails.getEmail() );
+            params.put( "videoViewDuration", String.valueOf(mVideoPlayedTime) );
+
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+
+        }
+
+
+
+        RequestQueue requestQueue = Volley.newRequestQueue( OnlinePlayerActivity.this );
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest( Request.Method.POST, "http://178.128.173.51:3000/saveViewInformation", params, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+
+
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+
+            }
+        } ) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                headers.put( "Content-Type", "application/json" );
+                return headers;
+            }
+        };
+
+        requestQueue.add( jsonObjectRequest );
+
+
 
     }
 
