ee2507826c345b7c3dbcd36f61e945fc09706b0b
unknown <mulaniimran27@gmail.com>
2019-04-24 15:58:37 +0530

    Update Box

---
+++ app/src/main/java/blockchainvideoapp/com/goviddo/goviddo/Fragments/TransactionFragment.java
@@ -0,0 +1,6 @@
+package blockchainvideoapp.com.goviddo.goviddo.Fragments;
+
+import android.app.Activity;
+
+public class TransactionFragment extends Activity {
+}
--- app/src/main/java/blockchainvideoapp/com/goviddo/goviddo/activity/HomeActivity.java
+++ app/src/main/java/blockchainvideoapp/com/goviddo/goviddo/activity/HomeActivity.java
@@ -1,6 +1,11 @@
 package blockchainvideoapp.com.goviddo.goviddo.activity;
 
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.BottomNavigationView;
@@ -19,13 +24,27 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import blockchainvideoapp.com.goviddo.goviddo.BuildConfig;
 import blockchainvideoapp.com.goviddo.goviddo.Fragments.HomeFragment;
 import blockchainvideoapp.com.goviddo.goviddo.Fragments.OthersFragment;
 import blockchainvideoapp.com.goviddo.goviddo.Fragments.RecentFragment;
@@ -37,6 +56,7 @@ public class HomeActivity extends AppCompatActivity  {
     Toolbar toolbar;
     FrameLayout frameLayout;
     BottomNavigationView bottomNavigationView;
+
 
 
     private final BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -78,6 +98,70 @@ public class HomeActivity extends AppCompatActivity  {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+
+        String update_url = "http://178.128.173.51:3000/updateApp";
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this );
+
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest( Request.Method.GET, update_url,  new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                System.out.println(response);
+                try {
+                    int version_code_current = response.getInt("version_code_current");
+                    int version_code_min = response.getInt("version_code_min");
+                    String update_info = response.getString("update_info");
+                    String update_date = response.getString("update_date");
+
+                    if(BuildConfig.VERSION_CODE <version_code_current){
+
+                        AlertDialog.Builder builder1 = new AlertDialog.Builder(HomeActivity.this);
+                        builder1.setMessage(update_info);
+                        builder1.setPositiveButton(
+                                "Update",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        final String appPackageName = getPackageName(); // getPackageName() from Context or Activity object
+                                        try {
+                                            startActivity(new Intent( Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appPackageName)));
+                                        } catch (android.content.ActivityNotFoundException anfe) {
+                                            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + appPackageName)));
+                                        }
+                                    }
+                                });
+
+                        AlertDialog alert11 = builder1.create();
+                        alert11.show();
+
+                        Button positiveButton = alert11.getButton(AlertDialog.BUTTON_POSITIVE);
+                        positiveButton.setTextColor( Color.parseColor("#FF0000"));
+
+
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }){
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                headers.put( "Content-Type", "application/json" );
+                return headers;
+            }
+
+        };
+
+        requestQueue.add( jsonObjectRequest );
+
 
 
 
@@ -134,6 +218,8 @@ public class HomeActivity extends AppCompatActivity  {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
 
 
